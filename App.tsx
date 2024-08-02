1408edd72dfc0c091d6ff6082b64a40f91741e14
msvalandro <msvalandro@proton.me>
2024-08-02 17:45:51 +0100

    feat: open app when notification clicked

--- App.tsx
+++ App.tsx
@@ -8,10 +8,11 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
-import { OneSignal } from 'react-native-onesignal';
+import { NotificationClickEvent, OneSignal } from 'react-native-onesignal';
 
 import { ONE_SIGNAL_APP_ID } from '@env'
 import { tagUserCreateEmail } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
 
 OneSignal.initialize(ONE_SIGNAL_APP_ID)
 OneSignal.Notifications.requestPermission(true)
@@ -20,6 +21,16 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserCreateEmail('john.doe@email.com')
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent) => {
+      console.log('Notificação aberta!');
+    };
+
+    OneSignal.Notifications.addEventListener('click', handleNotificationClick);
+
+    return () => OneSignal.Notifications.removeEventListener('click', handleNotificationClick);
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
